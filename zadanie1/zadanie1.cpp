60b43333724f9881c798757584840a4d8939ef32
KrystianWojewski <krystian.a.wojewski@gmail.com>
2022-10-10 14:16:36 +0200

    text center

--- zadanie1/zadanie1.cpp
+++ zadanie1/zadanie1.cpp
@@ -9,26 +9,28 @@
 
 int main(int argc, char **argv) {
     using namespace std;
-    std::ifstream file("data/zadanie1.json");
+    std::ifstream file(argv[1]);
     nlohmann::json data = nlohmann::json::parse(file);
     int h = data["h"];
     int w = data["w"];
+    int x =0;
 
     string text = data["text"];
+
+    int textCenter = text.size()/2;
 
     for (int i = 0; i < h; i++) {
         cout << "*";
         for (int l = 1; l < w - 1; l++) {
-            if (i == 1 && l <= text.size()) {
-                cout << text;
-                break;
+            if (i == (h-2)/2 && l >=  (w-2)/2 - textCenter && x <= text.size()) {
+                cout << text[x];
+                x++;
             }
             else if (i > 0 && i < h - 1) {
                 cout << " ";
             }
             else
-                cout << "*";
-        }
+                cout << "*";}
         cout << "*\n";
     }
     return 0;
