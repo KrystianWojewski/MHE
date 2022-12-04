e33aa76b62c70573248d236fe5efe6232cdb2052
s23215 <s23215@pjwstk.edu.pl>
2022-12-04 17:33:29 +0100

    lab6

---
+++ Nonogram/funcs.h
@@ -0,0 +1,22 @@
+#ifndef MHE_FUNCS_H
+#define MHE_FUNCS_H
+
+#include "struct.h"
+
+extern int calls;
+
+int evaluate_puzzle(int x, int y, const nonogram_t &nonogram);
+int count_inconsistent(const nonogram_t &nonogram);
+double evaluate(const nonogram_t &nonogram);
+bool next_solution(nonogram_t &nonogram);
+std::vector<nonogram_t> generate_neighbours(nonogram_t &p);
+std::vector<nonogram_t> generate_random_neighbours(const nonogram_t &p);
+nonogram_t generate_random_solution(nonogram_t &p);
+void print_f(std::string progress, std::string iter, bool show_progress, bool show_iterations);
+nonogram_t brute_force(nonogram_t nonogram_a, int iterations, bool show_progress, bool show_iterations);
+nonogram_t random_sampling(nonogram_t nonogram_a, int iterations, bool show_progress, bool show_iterations);
+nonogram_t hill_climb_det(nonogram_t start_nonogram, int iterations, bool show_progress, bool show_iterations);
+nonogram_t hill_climb_rand(nonogram_t start_nonogram, int iterations, bool show_progress, bool show_iterations);
+nonogram_t tabu_search(nonogram_t nonogram, int iterations, bool show_progress, bool show_iterations, int tabu_size);
+
+#endif
---
+++ Nonogram/struct.h
@@ -0,0 +1,43 @@
+#ifndef MHE_STRUCT_H
+#define MHE_STRUCT_H
+
+#include <vector>
+#include <iostream>
+
+struct nonogram_t {
+    int width;
+    int height;
+    std::vector<int> board;
+    int left_parmas_width;
+    int top_params_height;
+    std::vector<int> params_board;
+
+
+    int &get(const int x, const int y) {
+        return board[y * width + x];
+    }
+
+    int get_from_param(const int x, const int y) const {
+        if ((x < 0) || (x >= width + left_parmas_width) || (y < 0) || (y >= height + top_params_height)) {
+            throw std::invalid_argument("out of board");
+        } else if ((x >= left_parmas_width && y < top_params_height) ||
+                   (x < left_parmas_width && y >= top_params_height)) {
+            return params_board.at(y * (width + left_parmas_width) + x);
+        }
+        return 0;
+    }
+
+    int get_from_board(const int x, const int y) const {
+        if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
+            throw std::invalid_argument("out of board");
+            return 0;
+        } else {
+            return board.at(y * width + x);
+        }
+    }
+};
+
+std::ostream &operator<<(std::ostream &o, const nonogram_t &nonogram);
+bool operator==(nonogram_t l, nonogram_t r);
+
+#endif
